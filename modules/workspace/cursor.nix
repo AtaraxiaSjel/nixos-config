ab2b6ffd62bbd747add116af5c1982c2a41a3037
Dmitriy <alukard.develop@gmail.com>
2019-09-17 02:21:09 +0400

    delete unnecessary code

--- modules/workspace/cursor.nix
+++ modules/workspace/cursor.nix
@@ -6,10 +6,5 @@
   # Bibata_Amber, Bibata_Ice, Bibata_Oil
   home-manager.users.alukard.home.file.".icons/default" = {
     source = "${pkgs.bibata-cursors}/share/icons/Bibata_Oil";
-    onChange = ''
-      if [[ -v DISPLAY ]] ; then
-        $DRY_RUN_CMD ${pkgs.xorg.xrdb}/bin/xrdb -merge $HOME/.Xresources
-      fi
-    '';
   };
 }
