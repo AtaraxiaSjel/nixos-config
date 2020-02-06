d0ba2b31b9084409db5fa4072c44d8a786285b7f
Dmitriy Holkin <alukard.develop@gmail.com>
2020-02-07 03:52:17 +0400

    move hardware config

--- default.nix
+++ default.nix
@@ -1,26 +1,16 @@
 # This is AlukardBF's configuration file.
-#
 # Thanks for original config - balsoft.
-#
 # https://github.com/AlukardBF/nixos-config
-#
-# This is main nixos configuration
-# To use this configuration:
-#   1. Add your own secret.nix to this folder
-#   2. Replace /etc/nixos/configuration.nix with the following:
-#      import /path/to/this/nixos-config "Vendor-Type"
-#   3. Log in to application and services where neccesary
 
 
 device:
 { config, pkgs, lib, ... }:
 let sources = import ./nix/sources.nix;
 in {
-  imports =
-  [
-    /etc/nixos/hardware-configuration.nix
+  imports = [
+    "${./hardware-configuration}/${device}.nix"
     "${sources.home-manager}/nixos"
-    ./modules
+    (import ./modules device)
   ];
 
   inherit device;
