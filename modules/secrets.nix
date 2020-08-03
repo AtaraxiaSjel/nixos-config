1565ee0a23c2cfc3a35d77606259f0f4721e9a60
Dmitriy Kholkin <alukard.develop@gmail.com>
2020-08-04 01:44:50 +0400

    encrypt secret.nix with git-crypt

--- modules/secrets.nix
+++ modules/secrets.nix
@@ -2,17 +2,22 @@
 with lib;
 with types;
 let
+  secret = description:
+    mkOption {
+      inherit description;
+      type = nullOr str;
+    };
   mkCredOption = service: extra:
   mkOption {
     description = "Credentials for ${service}";
     type = nullOr (submodule {
       options = {
         user = mkOption {
-          type = string;
+          type = str;
           description = "Username for ${service}";
         };
         password = mkOption {
-          type = string;
+          type = str;
           description = "Password for ${service}";
         };
       } // extra;
@@ -21,17 +26,20 @@ let
 in rec {
   options.secrets = {
     wireguard = mkOption {
-      type = attrs;
       description = "Wireguard conf";
+      type = attrs;
     };
     windows-samba = mkCredOption "samba on windows" { };
     linux-samba = mkCredOption "samba on linux" { };
     spotify = mkCredOption "Spotify" { };
   };
   config = let
+    unlocked = import (pkgs.runCommand "check-secret" { }
+      "set +e; grep -qI . ${../secret.nix}; echo $? > $out") == 0;
     secretnix = import ../secret.nix;
-    secrets = if isNull secretnix then
-      mapAttrs (n: v: null) options.secrets
+    secrets = if !unlocked || isNull secretnix then
+      builtins.trace "secret.nix locked, building without any secrets"
+      (mapAttrs (n: v: null) options.secrets)
     else
       secretnix;
   in { inherit secrets; };
