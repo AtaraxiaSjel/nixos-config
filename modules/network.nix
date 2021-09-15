e9bdd97f775026bda5c524a376faf7d1e8f35647
Dmitriy Kholkin <alukard.develop@gmail.com>
2021-09-15 15:38:49 +0300

    add some hosts entries

--- modules/network.nix
+++ modules/network.nix
@@ -33,5 +33,15 @@ with config.deviceSpecific;
     firewall.enable = false;
     usePredictableInterfaceNames = true;
     hostName = config.device;
+
+    extraHosts = ''
+      0.0.0.0 log-upload-os.mihoyo.com
+      0.0.0.0 overseauspider.yuanshen.com
+      0.0.0.0 prd-lender.cdp.internal.unity3d.com
+      0.0.0.0 thind-prd-knob.data.ie.unity3d.com
+      0.0.0.0 thind-gke-usc.prd.data.corp.unity3d.com
+      0.0.0.0 cdp.cloud.unity3d.com
+      0.0.0.0 remote-config-proxy-prd.uca.cloud.unity3d.com
+    '';
   };
 }
