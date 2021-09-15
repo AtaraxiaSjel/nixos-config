5d58b2fc540438849685e4e0c337b62d4260ce16
Dmitriy Kholkin <alukard.develop@gmail.com>
2021-09-15 18:39:44 +0300

    fix ssh, gpg agent

--- modules/workspace/gpg.nix
+++ modules/workspace/gpg.nix
@@ -8,9 +8,8 @@
     services.gpg-agent = {
       enable = true;
       enableSshSupport = true;
-      pinentryFlavor = "gnome3";
+      pinentryFlavor = "gtk2";
       sshKeys = [
-        # "E6A6377C3D0827C36428A290199FDB3B91414AFE"
         "7A7130ABF128CC2C32B3D6AD27515056B0193CE1"
       ];
     };
