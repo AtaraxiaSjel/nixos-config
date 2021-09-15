9d5ff8962ac3cdc81254cba0ef7faa2521312fa0
Dmitriy Kholkin <alukard.develop@gmail.com>
2021-09-15 15:40:34 +0300

    fix picom settings

--- modules/workspace/picom.nix
+++ modules/workspace/picom.nix
@@ -13,14 +13,22 @@
         "100:_NET_WM_STATE@[2]:32a = '_NET_WM_STATE_FULLSCREEN'"
         "100:_NET_WM_STATE@[3]:32a = '_NET_WM_STATE_FULLSCREEN'"
         "100:_NET_WM_STATE@[4]:32a = '_NET_WM_STATE_FULLSCREEN'"
+        # Disable drawing underlying tabbed windows
+        "0:_NET_WM_STATE@[0]:32a *= '_NET_WM_STATE_HIDDEN'"
+        "0:_NET_WM_STATE@[1]:32a *= '_NET_WM_STATE_HIDDEN'"
+        "0:_NET_WM_STATE@[2]:32a *= '_NET_WM_STATE_HIDDEN'"
+        "0:_NET_WM_STATE@[3]:32a *= '_NET_WM_STATE_HIDDEN'"
+        "0:_NET_WM_STATE@[4]:32a *= '_NET_WM_STATE_HIDDEN'"
         # Other rules
         "100:class_i ?= 'vivaldi-stable'"
         "100:class_g = 'mpv'"
+        "100:class_g = 'explorer.exe'"
         "90:class_g = 'URxvt' && focused"
         "70:class_g = 'URxvt' && !focused"
       ];
       blur = true;
       blurExclude = [
+        "_NET_WM_STATE@:32a *= '_NET_WM_STATE_HIDDEN'"
         "class_g = 'i3-frame'"
         "class_g = 'slop'"
       ];
@@ -28,12 +36,16 @@
       vSync = true;
       experimentalBackends = true;
       extraOptions = ''
-        blur:
-        {
-          method = "gaussian";
-          size = 10;
-          deviation = 5.0;
-        };
+        blur-method = "gaussian";
+        blur-size = 10;
+        blur-deviation = 5.0;
+        # blur:
+        # {
+        #   method = "gaussian";
+        #   size = 10;
+        #   deviation = 5.0;
+        # };
+        unredir-if-possible = false;
       '';
     };
   };
