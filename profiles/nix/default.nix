83b84b990783898c5d2ad01fe8ee76b13c2216a7
Dmitriy Kholkin <alukard.develop@gmail.com>
2021-09-28 01:49:57 +0300

    enable cachix and patch nix

--- profiles/nix/default.nix
+++ profiles/nix/default.nix
@@ -1,7 +1,15 @@
 { config, lib, pkgs, inputs, ... }: {
   nix = rec {
     nixPath = lib.mkForce [ "self=/etc/self/compat" "nixpkgs=/etc/nixpkgs" ];
-    binaryCaches = [ "https://cache.nixos.org" ];
+    binaryCaches = [
+      "https://cache.nixos.org"
+      "https://nixos-rocm.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "nixos-rocm.cachix.org-1:VEpsf7pRIijjd8csKjFNBGzkBqOmw8H9PRmgAq14LnE="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
 
     registry.self.flake = inputs.self;
     registry.nixpkgs.flake = inputs.nixpkgs;
@@ -13,7 +21,7 @@
     autoOptimiseStore = false;
 
     package = inputs.nix.packages.x86_64-linux.nix.overrideAttrs (oa: {
-      patches = [./nix.patch] ++ oa.patches or [];
+      patches = [ ./nix.patch ./flakes.patch ] ++ oa.patches or [];
     });
 
     extraOptions = ''
