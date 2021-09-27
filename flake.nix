bc47b4f86ee2e534238e4fdaf602bd4b886df386
Dmitriy Kholkin <alukard.develop@gmail.com>
2021-09-28 01:37:20 +0300

    update flakes

--- flake.nix
+++ flake.nix
@@ -5,44 +5,25 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-master.url = "github:nixos/nixpkgs/master";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-21.05";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    # base16.url = "/shared/nixos/base16-nix";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     base16.url = "github:alukardbf/base16-nix";
     base16-horizon-scheme = {
       url = "github:michael-ball/base16-horizon-scheme";
       flake = false;
     };
-    materia-theme = {
-      url = "github:nana-4/materia-theme";
-      flake = false;
-    };
-    zsh-autosuggestions = {
-      url = "github:zsh-users/zsh-autosuggestions";
-      flake = false;
-    };
-    zsh-nix-shell = {
-      url = "github:chisui/zsh-nix-shell";
-      flake = false;
-    };
-    zsh-you-should-use = {
-      url = "github:MichaelAquilina/zsh-you-should-use";
-      flake = false;
-    };
-    zsh-cod = {
-      url = "github:dim-an/cod";
+    base16-tokyonight-scheme = {
+      url = "github:alukardbf/base16-tokyonight-scheme";
       flake = false;
     };
     i3lock-fancy-rapid = {
       url = "github:yvbbrjdr/i3lock-fancy-rapid";
       flake = false;
     };
-    nixpkgs-mozilla = {
-      url = "github:mozilla/nixpkgs-mozilla";
-      flake = false;
-    };
-    rycee = {
-      url = "gitlab:rycee/nur-expressions";
+    materia-theme = {
+      url = "github:nana-4/materia-theme";
       flake = false;
     };
     multimc-cracked = {
@@ -53,11 +34,43 @@
       submodules = true;
       type = "git";
     };
+    nixos-rocm = {
+      url = "github:nixos-rocm/nixos-rocm/4.3.x";
+      # inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs-mozilla = {
+      url = "github:mozilla/nixpkgs-mozilla";
+      flake = false;
+    };
+    nix-direnv = {
+      url = "github:nix-community/nix-direnv";
+      flake = false;
+      # inputs.nixpkgs.follows = "nixpkgs";
+    };
     qbittorrent-ee = {
       url = "github:c0re100/qBittorrent-Enhanced-Edition";
       flake = false;
     };
-    nix-direnv.url = "github:nix-community/nix-direnv";
+    rycee = {
+      url = "gitlab:rycee/nur-expressions";
+      flake = false;
+    };
+    zsh-autosuggestions = {
+      url = "github:zsh-users/zsh-autosuggestions";
+      flake = false;
+    };
+    zsh-cod = {
+      url = "github:dim-an/cod";
+      flake = false;
+    };
+    zsh-nix-shell = {
+      url = "github:chisui/zsh-nix-shell";
+      flake = false;
+    };
+    zsh-you-should-use = {
+      url = "github:MichaelAquilina/zsh-you-should-use";
+      flake = false;
+    };
   };
 
   outputs = { nixpkgs, nix, self, ... }@inputs:
