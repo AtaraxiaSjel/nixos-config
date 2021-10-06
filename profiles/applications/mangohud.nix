0b92be3df8e7370789496abfc4a0052ef12a02ba
Dmitriy Kholkin <alukard.develop@gmail.com>
2021-10-06 23:44:35 +0300

    change mangohud config

--- profiles/applications/mangohud.nix
+++ profiles/applications/mangohud.nix
@@ -2,20 +2,14 @@
   home-manager.users.alukard = lib.mkIf config.deviceSpecific.isGaming {
     home.packages = [ pkgs.mangohud ];
     xdg.configFile."MangoHud/MangoHud.conf".source = pkgs.writeText "MangoHud.conf" ''
+      fps_limit=60
       toggle_fps_limit=F1
       legacy_layout=false
       gpu_stats
       gpu_temp
-      gpu_load_change
-      gpu_load_value=50,90
-      gpu_load_color=FFFFFF,FF7800,CC0000
       gpu_text=GPU
       cpu_stats
       cpu_temp
-      cpu_load_change
-      core_load_change
-      cpu_load_value=50,90
-      cpu_load_color=FFFFFF,FF7800,CC0000
       cpu_color=2e97cb
       cpu_text=CPU
       io_color=a491d3
@@ -24,11 +18,15 @@
       ram
       ram_color=c26693
       fps
+      engine_version
       engine_color=eb5b5b
       gpu_color=2e9762
+      wine
       wine_color=eb5b5b
       frame_timing=1
       frametime_color=00ff00
+      resolution
+      vkbasalt
       media_player_color=ffffff
       time
       background_alpha=0.4
