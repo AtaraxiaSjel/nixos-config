88490617800c6f1b87cbf7f310bf4bc69f837623
Dmitriy Kholkin <alukard.develop@gmail.com>
2021-10-06 23:46:57 +0300

    fix df script

--- profiles/workspace/i3status-rust/scripts/df.nix
+++ profiles/workspace/i3status-rust/scripts/df.nix
@@ -1,4 +1,4 @@
 { iconfont, ... }: ''
   #!/usr/bin/env bash
-  echo "<span font="${iconfont} Solid"></span> `_ btrfs fi usage / | head -n7 | tail -n1 | awk '{print $3}'`"
+  echo '<span font="${iconfont} Solid"></span>' `(sudo btrfs fi usage / | head -n7 | tail -n1 | awk '{print $3}')`
 ''
