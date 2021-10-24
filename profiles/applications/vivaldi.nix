30298360a7199c80cc9c98d4d4d236a360c2ccb0
Dmitriy Kholkin <alukard.develop@gmail.com>
2021-10-24 23:15:45 +0300

    add custom.css to vivaldi

--- profiles/applications/vivaldi.nix
+++ profiles/applications/vivaldi.nix
@@ -4,7 +4,46 @@
     desktop = "vivaldi";
   };
 
-  home-manager.users.alukard.home.packages = with pkgs; [
-    (vivaldi.override { proprietaryCodecs = true; })
-  ];
+  home-manager.users.alukard = {
+    home.packages = with pkgs; [
+      (vivaldi.override { proprietaryCodecs = true; })
+    ];
+
+    xdg.configFile."vivaldi/css/custom.css".source = pkgs.writeText "custom.css" ''
+      #header {
+          min-height: 0;
+          z-index: auto;
+      }
+      .vivaldi, .window-buttongroup {
+          z-index: 999;
+      }
+      .vivaldi {
+          position: relative !important;
+          top: 3px !important;
+      }
+      .topmenu {
+          position: absolute;
+      }
+      .topmenu>nav {
+          left: -28px;
+          top: 4px;
+      }
+      .horizontal-menu .toolbar.toolbar-addressbar {
+          padding: 0 150px 0 269px !important;
+      }
+      .toolbar.toolbar-addressbar {
+          padding: 0 150px 0 50px !important;
+          height: 36px;
+      }
+      .bookmark-bar {
+          margin-bottom: 0;
+      }
+      .window-buttongroup {
+          display: none !important;
+      }
+      .UrlBar {
+          margin-left: 34px;
+      }
+    '';
+  };
 }
