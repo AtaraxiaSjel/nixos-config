444979e465152b220c3d010a3f095fe2bf9eba20
Dmitriy Kholkin <alukard.develop@gmail.com>
2021-10-25 23:59:11 +0300

    add swap to oracle

--- machines/Oracle-Cloud/hardware-configuration.nix
+++ machines/Oracle-Cloud/hardware-configuration.nix
@@ -9,4 +9,8 @@
   fileSystems."/boot" = { device = "/dev/disk/by-uuid/C8C5-C634"; fsType = "vfat"; };
   boot.initrd.kernelModules = [ "nvme" ];
   fileSystems."/" = { device = "/dev/sda3"; fsType = "xfs"; };
+  swapDevices = [ {
+      device = "/dev/disk/by-partuuid/d4aa8434-9803-45ac-9983-07e10e1409b4";
+      randomEncryption.enable = true;
+  } ];
 }
