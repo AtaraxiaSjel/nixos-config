111428a82f29e48274f85913ed0f5d8497cb251e
Dmitriy Kholkin <AlukardBF@users.noreply.github.com>
2022-08-24 18:33:53 +0300

    fix iso and some cleanup

--- machines/Flakes-ISO/default.nix
+++ machines/Flakes-ISO/default.nix
@@ -1,80 +1,22 @@
 { modulesPath, lib, inputs, pkgs, ... }: {
   imports = with inputs.self.nixosModules; with inputs.self.nixosProfiles; [
-    "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
-    "${modulesPath}/profiles/qemu-guest.nix"
-    inputs.self.nixosRoles.base
-    inputs.base16.hmModule
-
-    xray
-    applications-setup
-    sound
-    themes
-
-    firefox
-    kitty
-    mpv
-    packages
-    rofi
-    vscode
-
-    copyq
-    cursor
-    direnv
-    fonts
-    gtk
-    kde
-    mako
-    nix-index
-    print-scan
-    proxy
-    hyprland
-    waybar
-
-
-    # applications-setupsetup
-    # cursor
-    # fonts
-    # gtk
-    # i3status-rust
-    # kde
-    # kitty
-    # mako
-    # mpv
-    # packages
-    # print-scan
-    # rofi
-    # sound
-    # sway
-    # themes
-    # vivaldi
-    # vscode
-    # kitty
-    # mako
-    # mpv
-    # packages
-    # print-scan
-    # rofi
-    # sound
-    # sway
-    # themes
-    # vivaldi
-    # vscode
+    "${modulesPath}/installer/cd-dvd/installation-cd-graphical-plasma5.nix"
   ];
-  disabledModules = [ "installer/cd-dvd/channel.nix" ];
-  networking.networkmanager.enable = lib.mkForce true;
-  networking.wireless.enable = lib.mkForce false;
-  services.getty.autologinUser = lib.mkForce "alukard";
-  services.openssh.permitRootLogin = lib.mkForce "no";
-  # sound.enable = lib.mkForce false;
 
-  deviceSpecific.devInfo.drive.type = "hdd";
-  deviceSpecific.devInfo.gpu.vendor = "other";
-  deviceSpecific.isHost = false;
-  deviceSpecific.isShared = false;
-  deviceSpecific.isGaming = false;
-  deviceSpecific.enableVirtualisation = false;
-  deviceSpecific.wireguard.enable = false;
-  deviceSpecific.isServer = false;
+  options.device = lib.mkOption { type = lib.types.str; };
 
-  isoImage.volumeID = lib.mkForce "NIXOS_ISO";
+  config = {
+    environment.systemPackages = [ pkgs.git ];
+    nix = {
+      nixPath = lib.mkForce [ "self=/etc/self/compat" "nixpkgs=/etc/nixpkgs" ];
+      registry.self.flake = inputs.self;
+      registry.nixpkgs.flake = inputs.nixpkgs;
+      extraOptions = ''
+        experimental-features = nix-command flakes
+      '';
+    };
+    environment.etc.nixpkgs.source = inputs.nixpkgs;
+    environment.etc.self.source = inputs.self;
+  };
+  # isoImage.volumeID = lib.mkForce "NIXOS_ISO";
 }
