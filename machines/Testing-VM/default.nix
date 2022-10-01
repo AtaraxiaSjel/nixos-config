882ba0b241ffaba4aa0418f6a98a06cfd0e41877
Dmitriy Kholkin <AlukardBF@users.noreply.github.com>
2022-10-01 22:50:00 +0300

    add testing-vm

--- machines/Testing-VM/default.nix
+++ machines/Testing-VM/default.nix
@@ -1,8 +1,16 @@
-{ inputs, modulesPath, lib, ... }: {
-  imports = [
+{ inputs, modulesPath, config, pkgs, lib, ... }: {
+  imports = with inputs.self.nixosModules; with inputs.self.nixosProfiles; [
     "${modulesPath}/profiles/qemu-guest.nix"
-    ./hardware-configuration.nix
-    inputs.self.nixosRoles.desktop
+    ./qemu-vm.nix
+    # ./hardware-configuration.nix
+    inputs.self.nixosRoles.base
+    inputs.base16.hmModule
+
+    direnv
+    firefox
+    fonts
+    themes
+    vscode
   ];
 
   deviceSpecific.devInfo = {
@@ -30,5 +38,33 @@
 
   hardware.video.hidpi.enable = lib.mkForce false;
 
-  boot.kernelPackages = lib.mkForce boot.zfs.package.latestCompatibleLinuxPackages;
+  boot.kernelPackages = lib.mkForce config.boot.zfs.package.latestCompatibleLinuxPackages;
+  services.xserver = {
+    # enable = false;
+    enable = true;
+    displayManager.sddm.enable = true;
+    desktopManager.plasma5.enable = true;
+  };
+  # services.greetd = {
+  #   enable = true;
+  #   package = pkgs.greetd.gtkgreet;
+  #   settings = {
+  #     default_session = {
+  #       command = "${pkgs.cage}/bin/cage -s -- gtkgreet";
+  #     };
+  #   };
+  # };
+  # services.greetd = {
+  #   enable = true;
+  #   package = pkgs.greetd.tuigreet;
+  #   settings = {
+  #     default_session = {
+  #       # command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-session \"${pkgs.plasma5Packages.plasma-workspace}/share/wayland-sessions\"";
+  #       command = "${pkgs.greetd.tuigreet}/bin/tuigreet";
+  #       # user = "alukard";
+  #     };
+  #   };
+  # };
+  networking.usePredictableInterfaceNames = lib.mkForce false;
+  # environment.systemPackages = with pkgs; [ firefox ];
 }
