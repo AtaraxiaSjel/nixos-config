dcb6990270684ac289d867b9c9a3a0f628e5b3d1
Dmitriy Kholkin <AlukardBF@users.noreply.github.com>
2022-10-06 00:44:52 +0300

    gdrive mount wip

---
+++ profiles/applications/google-drive.nix
@@ -0,0 +1,20 @@
+{ config, lib, pkgs, ... }: {
+  home-manager.users.alukard = {
+    home.packages = [ pkgs.google-drive-ocamlfuse ];
+    # systemd.user.services.google-drive-ocamlfuse = {
+    #   Service = {
+    #     ExecStart = "${pkgs.google-drive-ocamlfuse}/bin/google-drive-ocamlfuse";
+    #     Type = "simple";
+    #   };
+    #   Unit = rec {
+    #     After = [ "network-online.target" ];
+    #     Wants = After;
+    #   };
+    #   Install.WantedBy = [ "multi-user.target" ];
+    #   # Unit = {
+    #   #   PartOf = [ "graphical-session-pre.target" ];
+    #   # };
+    #   # Install.WantedBy = [ "graphical-session-pre.target" ];
+    # };
+  };
+}
