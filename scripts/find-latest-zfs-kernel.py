1f6e559f101d078228ffdbc732d61fa651f6731b
Dmitriy Kholkin <AlukardBF@users.noreply.github.com>
2022-10-06 00:42:47 +0300

    scripts for generate zen kernel patch

---
+++ scripts/find-latest-zfs-kernel.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i python3 -p python3
+
+import sys
+import re
+
+if __name__ == "__main__":
+    file = sys.argv[1]
+    results = []
+    with open(file, "r") as lines:
+        for line in lines:
+            if "kernel.kernelOlder" in line:
+                results.append(line)
+    line = results[-2].strip()
+    pattern = re.compile(r"\d\.\d{1,2}")
+    version = pattern.search(line).group(0)
+    major, minor = version.split('.')
+    if int(minor) > 1:
+        minor = int(minor) - 1
+    version = f"{major}.{minor}"
+    print(version)
