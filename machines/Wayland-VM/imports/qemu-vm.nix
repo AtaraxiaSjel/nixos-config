90b9e9d32b7e5ad912f043d032d8b6155b8623ed
Dmitriy Kholkin <AlukardBF@users.noreply.github.com>
2022-10-06 00:43:34 +0300

    wayland-vm

---
+++ machines/Wayland-VM/imports/qemu-vm.nix
@@ -0,0 +1,17 @@
+{ modulesPath, config, ... }: {
+  imports = [
+    "${modulesPath}/virtualisation/qemu-vm.nix"
+  ];
+  virtualisation = {
+    qemu.options = [ "-vga none" "-device virtio-vga-gl" "-display gtk,gl=on" ];
+    cores = 4;
+    memorySize = 6044;
+    msize = 131072;
+    diskSize = 20480;
+    diskImage = "./vm-images/${config.device}.qcow2";
+    # resolution = { x = 1920; y = 1080; };
+
+    useNixStoreImage = true;
+    writableStore = false;
+  };
+}
