b2dccbed8f50a2bcc33f4f13e8702bdb34f92a35
Dmitriy Kholkin <AlukardBF@users.noreply.github.com>
2022-10-21 14:01:19 +0300

    vfio passthrough module

--- machines/AMD-Workstation/default.nix
+++ machines/AMD-Workstation/default.nix
@@ -2,6 +2,8 @@
   imports = [
     ./hardware-configuration.nix
     inputs.self.nixosRoles.workstation
+
+    # inputs.self.nixosModules.passthrough
   ];
 
   deviceSpecific.devInfo = {
@@ -35,5 +37,63 @@
 
   home-manager.users.alukard.home.packages = lib.mkIf config.deviceSpecific.enableVirtualisation [
     inputs.nixos-generators.packages.${pkgs.system}.nixos-generators
+
+    # pkgs.looking-glass-client
   ];
+
+  # VFIO Passthough
+  # virtualisation = {
+    # sharedMemoryFiles = {
+      # # scream = {
+      # #   user = "alukard";
+      # #   group = "qemu-libvirtd";
+      # #   mode = "666";
+      # # };
+      # looking-glass = {
+        # user = "alukard";
+        # group = "libvirtd";
+        # mode = "666";
+      # };
+    # };
+    # libvirtd = {
+      # enable = true;
+      # qemu = {
+        # ovmf.enable = true;
+        # runAsRoot = lib.mkForce true;
+      # };
+#
+      # onBoot = "ignore";
+      # onShutdown = "shutdown";
+#
+      # clearEmulationCapabilities = false;
+#
+      # deviceACL = [
+        # # "/dev/input/by-path/pci-0000:0b:00.3-usb-0:2.2.4:1.0-event-mouse" # Trackball
+        # # "/dev/input/by-path/pci-0000:0b:00.3-usb-0:2.2.3:1.0-event-kbd" # Tastatur
+        # # "/dev/input/by-path/pci-0000:0b:00.3-usb-0:2.2.3:1.1-event-mouse" # Tastatur
+        # # "/dev/input/by-path/pci-0000:0b:00.3-usb-0:2.2.3:1.1-mouse" # Tastatur
+        # "/dev/vfio/vfio"
+        # "/dev/vfio/17"
+        # "/dev/kvm"
+        # # "/dev/shm/scream"
+        # "/dev/shm/looking-glass"
+      # ];
+    # };
+    # vfio = {
+      # enable = true;
+      # IOMMUType = "amd";
+      # # group 17: 0b:00.0 and 0b:00.1
+      # devices = [ "10de:1244" "10de:0bee" ];
+      # blacklistNvidia = true;
+      # disableEFIfb = false;
+      # ignoreMSRs = true;
+      # applyACSpatch = false;
+    # };
+    # hugepages = {
+      # enable = true;
+      # defaultPageSize = "1G";
+      # pageSize = "1G";
+      # numPages = 6;
+    # };
+  # };
 }
