9687c5a356c1f02e6872c702bf4b8d70edade198
Dmitriy Kholkin <ataraxiadev@ataraxiadev.com>
2023-03-23 01:53:09 +0300

    auto-mount usb disk on startup

---
+++ machines/Home-Hypervisor/disks.nix
@@ -0,0 +1,23 @@
+{ config, lib, ... }: {
+  boot.initrd = rec {
+    luks.devices = {
+      "crypt-nas" = {
+        device = "/dev/disk/by-id/usb-JMicron_Tech_A311737E-0:0";
+        keyFile = "/nas_keyfile0.bin";
+      };
+    };
+    secrets = {
+      "nas_keyfile0.bin" = "/etc/secrets/nas_keyfile0.bin";
+    };
+    availableKernelModules = [
+      "usb_storage"
+      "usbcore"
+      "scsi_mod"
+      "usb_common"
+      "ehci_pci" "ahci" "uas" "sd_mod" "sdhci_pci"
+    ];
+    kernelModules = availableKernelModules;
+  };
+
+  boot.zfs.extraPools = [ "nas-pool" ];
+}
