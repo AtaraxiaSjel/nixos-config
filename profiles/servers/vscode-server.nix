686d345849355f3e41738740af880531eed823bf
Dmitriy Kholkin <ataraxiadev@ataraxiadev.com>
2023-03-23 01:51:53 +0300

    update vscode-server

--- profiles/servers/vscode-server.nix
+++ profiles/servers/vscode-server.nix
@@ -1,19 +1,8 @@
 { config, lib, pkgs, inputs, ... }: {
-  home-manager.sharedModules = [
-    inputs.vscode-server-fixup.nixosModules.home-manager.nixos-vscode-server
-  ];
-
-  home-manager.users.${config.mainuser} = let
-    extensions = builtins.tryEval config.home-manager.users.${config.mainuser}.programs.vscode.extensions;
-  in {
-    services.vscode-server = {
-      enable = true;
-      extensions = if extensions.success then extensions.value
-      else with inputs.nix-vscode-marketplace.packages.${pkgs.system}.vscode; [
-        bbenoist.nix
-      ];
-      immutableExtensionsDir = true;
-    };
+  services.vscode-server = {
+    enable = true;
+    nodejsPackage = pkgs.nodejs-16_x;
+    #installPath = "~/.vscode-server-oss";
   };
 
   persist.state.homeDirectories = [ ".vscode-server" ];
