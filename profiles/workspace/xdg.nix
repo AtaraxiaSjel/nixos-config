46588dfba3a35cb18197e967edb5f028383b6103
Dmitriy Kholkin <ataraxiadev@ataraxiadev.com>
2023-04-15 03:21:02 +0300

    add auto-clean downloads folder

--- profiles/workspace/xdg.nix
+++ profiles/workspace/xdg.nix
@@ -4,15 +4,24 @@
     xdg.userDirs.enable = true;
   };
 
-  environment.sessionVariables = {
-    DE = "generic";
+  environment.sessionVariables = { DE = "generic"; };
+
+  systemd.user.services.cleanup-home-dirs = let
+    home-conf = config.home-manager.users.${config.mainuser};
+    days = "30";
+    folders = map (x: home-conf.home.homeDirectory + "/" + x) [ "Downloads" ];
+  in {
+    serviceConfig.Type = "oneshot";
+    script = ''
+      ${builtins.concatStringsSep "\n" (map (x:
+        "find ${
+          lib.escapeShellArg x
+        } -mtime +${days} -exec rm -rv {} + -depth;")
+        folders)}
+    '';
+    wantedBy = [ "default.target" ];
   };
 
-  persist.state.homeDirectories = [
-    "Documents"
-    "Downloads"
-    "Music"
-    "Pictures"
-    "Videos"
-  ];
+  persist.state.homeDirectories =
+    [ "Documents" "Downloads" "Music" "Pictures" "Videos" ];
 }
