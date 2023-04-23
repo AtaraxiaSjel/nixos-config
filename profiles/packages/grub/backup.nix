a52e050a702e8b786bea64fec65a6dd1cce43c6f
André Vitor de Lima Matos <andre.vmatos@gmail.com>
2023-04-23 18:10:26 +0000

    Update grub2 with argon2 on backup.nix
    This uses the latest grub2-git commit, plus patches in AUR's grub-improved-luks2-git. Notice `type-fix.patch` is due to outdated gnulib, but that can't be on latest commit since it breaks build; the commit I used in let/in makes it work.
    This is done in a way to keep maximum compatibility with nixpkgs's grub2 package.

--- profiles/packages/grub/backup.nix
+++ profiles/packages/grub/backup.nix
@@ -1,111 +1,78 @@
-{ ... }: {}
-# grub2 = prev.grub2.overrideAttrs (oa: {
-#   version = "2.06.r291";
-#   src = prev.fetchgit {
-#     url = "https://git.savannah.gnu.org/git/grub.git";
-#     rev = "e43f3d93b28cce852c110c7a8e40d8311bcd8bb1";
-#     hash = "sha256-8M0WqeDE4Hrwq/zlygfbAWUt7vdDeqfJLX1ADzQGM3I=";
-#   };
-#   patches = [
-#     ./packages/grub/fix-bash-completion.patch
-#     ./packages/grub/add-hidden-menu-entries.patch
-#     ./packages/grub/license.patch
-#     ./packages/grub/5000-grub-2.06-luks2-argon2-v4.patch
-#     ./packages/grub/9500-grub-AUR-improved-luks2.patch
-#   ];
+final: prev: {
+  grub2 = prev.grub2.overrideAttrs (attrs: {
+    version = "2.06.r499.ge67a551a4";
 
-#   # GRUB_AUTOGEN="1";
+    src = prev.fetchgit {
+      url = "https://git.savannah.gnu.org/git/grub.git";
+      rev = "e67a551a48192a04ab705fca832d82f850162b64";
+      hash = "sha256-HycIXy8qf56JVQP5KUavfNShyU0hE+/HrdbT/ZBnzzI=";
+    };
 
-#   nativeBuildInputs = with prev; [ bison flex python3 pkg-config gettext freetype autoconf automake ];
-#   # nativeBuildInputs = oa.nativeBuildInputs ++ [
-#     # prev.autoconf prev.automake
-#   #   prev.autogen
-#   #   # prev.libargon2
-#   # ];
+    patches = [
+      ./fix-bash-completion.patch
+      (prev.fetchpatch {
+        name = "Add-hidden-menu-entries.patch";
+        # https://lists.gnu.org/archive/html/grub-devel/2016-04/msg00089.html
+        url = "https://marc.info/?l=grub-devel&m=146193404929072&q=mbox";
+        sha256 = "00wa1q5adiass6i0x7p98vynj9vsz1w0gn1g4dgz89v35mpyw2bi";
+      })
 
-#   # buildInputs = oa.buildInputs ++ [ prev.libargon2 ];
-#   # CPPFLAGS="-O2";
-#   # NIX_CFLAGS_COMPILE = "-Wno-error -O2";
+      # argon2 patches from AUR: https://aur.archlinux.org/packages/grub-improved-luks2-git
+      (prev.fetchpatch {
+        name = "argon_1.patch";
+        url = "https://aur.archlinux.org/cgit/aur.git/plain/argon_1.patch?h=grub-improved-luks2-git";
+        sha256 = "sha256-WCt+sVr8Ss/bAI41yMJmcZoIPVO1HFEjw1OVRUPYb+w=";
+      })
+      (prev.fetchpatch {
+        name = "argon_2.patch";
+        url = "https://aur.archlinux.org/cgit/aur.git/plain/argon_2.patch?h=grub-improved-luks2-git";
+        sha256 = "sha256-OMQYjTFq0PpO38wAAXRsYUfY8nWoAMcPhKUlbqizIS8=";
+      })
+      (prev.fetchpatch {
+        name = "argon_3.patch";
+        url = "https://aur.archlinux.org/cgit/aur.git/plain/argon_3.patch?h=grub-improved-luks2-git";
+        sha256 = "sha256-rxtvrBG4HhGYIvpIGZ7luNH5GPbl7TlqbNHcnR7IZc8=";
+      })
+      (prev.fetchpatch {
+        name = "argon_4.patch";
+        url = "https://aur.archlinux.org/cgit/aur.git/plain/argon_4.patch?h=grub-improved-luks2-git";
+        sha256 = "sha256-Hz88P8T5O2ANetnAgfmiJLsucSsdeqZ1FYQQLX0WP3I=";
+      })
+      (prev.fetchpatch {
+        name = "argon_5.patch";
+        url = "https://aur.archlinux.org/cgit/aur.git/plain/argon_5.patch?h=grub-improved-luks2-git";
+        sha256 = "sha256-cs5dKI2Am+Kp0/ZqSWqd2h/7Oj+WEBeKgWPVsCeMgwk=";
+      })
+      (prev.fetchpatch {
+        name = "grub-install_luks2.patch";
+        url = "https://aur.archlinux.org/cgit/aur.git/plain/grub-install_luks2.patch?h=grub-improved-luks2-git";
+        sha256 = "sha256-I+1Yl0DVBDWFY3+EUPbE6FTdWsKH81DLP/2lGPVJtLI=";
+      })
+    ];
+    nativeBuildInputs = (builtins.filter (x: x.name != "autoreconf-hook") attrs.nativeBuildInputs) ++ (with final; [autoconf automake]);
 
-#   # configureFlags = [
-#   #   "GRUB_ENABLE_CRYPTODISK=y"
-#   #   "TARGET_CFLAGS=-O2"
-#   #   "--disable-werror"
-#   #   "CFLAGS=\"\${CFLAGS/-fno-plt}\""
-#   #   "CPPFLAGS=\"-O2\""
-#   #   "--disable-silent-rules"
-#   #   "--disable-werror"
-#   # ] ++ oa.configureFlags;
-#   # NIX_CFLAGS_COMPILE = "-O2";
-#   # CFLAGS="-fno-plt";
-#   # configureFlags = oa.configureFlags ++ [
-#   #   # "--enable-boot-time"
-#   #   # "--enable-cache-stats"
-#   #   # "--enable-device-mapper"
-#   #   # "--enable-grub-mkfont"
-#   #   # "--enable-grub-mount"
-#   #   # "--enable-mm-debug"
-#   #   "--disable-silent-rules"
-#   #   "--disable-werror"
-#   #   # "CPPFLAGS=\"$CPPFLAGS -O2\""
-#   # ];
-#   preConfigure = with prev; ''
-#     for i in "tests/util/"*.in
-#     do
-#       sed -i "$i" -e's|/bin/bash|${stdenv.shell}|g'
-#     done
-#     # Apparently, the QEMU executable is no longer called
-#     # `qemu-system-i386', even on i386.
-#     #
-#     # In addition, use `-nodefaults' to avoid errors like:
-#     #
-#     #  chardev: opening backend "stdio" failed
-#     #  qemu: could not open serial device 'stdio': Invalid argument
-#     #
-#     # See <http://www.mail-archive.com/qemu-devel@nongnu.org/msg22775.html>.
-#     sed -i "tests/util/grub-shell.in" \
-#         -e's/qemu-system-i386/qemu-system-x86_64 -nodefaults/g'
-#     unset CPP # setting CPP intereferes with dependency calculation
+    preConfigure = let
+      gnulib = final.fetchgit {
+        url = "https://git.savannah.gnu.org/r/gnulib.git";
+        rev = "06b2e943be39284783ff81ac6c9503200f41dba3";
+        sha256 = "sha256-xhxN8Tw15ENAMSE/cTkigl5yHR3T2d7B1RMFqiMvmxU=";
+      };
+    in
+      builtins.replaceStrings ["patchShebangs ."] [
+        ''
+          patchShebangs .
 
-#     patchShebangs .
+          ./bootstrap --no-git --gnulib-srcdir=${gnulib}
+        ''
+      ]
+      attrs.preConfigure;
 
-#     ./bootstrap --no-git --gnulib-srcdir=${gnulib} # my changes
-
-#     substituteInPlace ./configure --replace '/usr/share/fonts/unifont' '${unifont}/share/fonts'
-
-#     sed -i 's/idx_t/grub_size_t/g' ./grub-core/disk/luks2.c # my changes
-#   '';
-#   # preConfigure = oa.preConfigure + ''
-#   #   echo "I'm here!"
-#   #   ./bootstrap --no-git --gnulib-srcdir=${prev.gnulib}
-#   # '';
-#   # postPatch = let
-#   #   bash-patch = ./packages/grub/fix-bash-completion.patch;
-#   #   menu-patch = ./packages/grub/add-hidden-menu-entries.patch;
-#   #   # alloc-patch = ./packages/grub/4500-grub-2.06-runtime-memregion-alloc.patch;
-#   #   luks-argon2-patch = ./packages/grub/5000-grub-2.06-luks2-argon2-v4.patch;
-#   #   luks2-patch = ./packages/grub/9500-grub-AUR-improved-luks2.patch;
-#   #   argon1-patch = ./packages/grub/argon_1.patch;
-#   #   argon2-patch = ./packages/grub/argon_2.patch;
-#   #   argon3-patch = ./packages/grub/argon_3.patch;
-#   #   argon4-patch = ./packages/grub/argon_4.patch;
-#   #   argon5-patch = ./packages/grub/argon_5.patch;
-#   # in ''
-#   #   patch -Np1 -i "${bash-patch}"
-#   #   patch -Np1 -i "${menu-patch}"
-#   #   # patch -Np1 -i "${argon1-patch}"
-#   #   # patch -Np1 -i "${argon2-patch}"
-#   #   # patch -Np1 -i "${argon3-patch}"
-#   #   # patch -Np1 -i "${argon4-patch}"
-#   #   # patch -Np1 -i "${argon5-patch}"
-#   #   patch -Np1 -i "${luks-argon2-patch}"
-#   #   patch -Np1 -i "${luks2-patch}"
-#   #   # ls -lah ./
-#   #   # ls -lah ./grub-core
-#   #   # echo "CFLAGS"
-#   #   # echo $CFLAGS
-#   #   # sed -i 's#rm -f kernel_syms.input#cat kernel_syms.input; rm -f kernel_syms.input#' ./grub-core/Makefile.am
-#   #   # sed -i 's#cat $<#cat $<\n\tcat $<#' ./grub-core/Makefile.am
-#   #   # exit 1
-#   # '' + oa.postPatch;
-# });
+    configureFlags =
+      attrs.configureFlags
+      ++ [
+        "--disable-nls"
+        "--disable-silent-rules"
+        "--disable-werror"
+      ];
+  });
+}
