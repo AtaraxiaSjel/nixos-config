48d9862e5937ee556eec79062ed23cc42f6bee17
Dmitriy Kholkin <ataraxiadev@ataraxiadev.com>
2023-06-13 04:24:05 +0300

    add new hoyolab account

--- profiles/servers/hoyolab.nix
+++ profiles/servers/hoyolab.nix
@@ -1,11 +1,14 @@
 { config, pkgs, lib, ... }: {
-  secrets.hoyolab-cookie1 = { };
-  secrets.hoyolab-cookie2 = { };
+  secrets.hoyolab-cookie1.services = [ ];
+  secrets.hoyolab-cookie2.services = [ ];
+  secrets.hoyolab-cookie3.services = [ ];
+  
   services.hoyolab-daily-bot = {
     enable = true;
     cookieFiles = [
       config.secrets.hoyolab-cookie1.decrypted
       config.secrets.hoyolab-cookie2.decrypted
+      config.secrets.hoyolab-cookie3.decrypted
     ];
   };
 }
