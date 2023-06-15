5930c3233ca77a86bfd1fba714c44b27b7696c6f
Dmitriy Kholkin <ataraxiadev@ataraxiadev.com>
2023-06-16 02:30:19 +0300

    fix ci

--- ci.nix
+++ ci.nix
@@ -1,7 +1,42 @@
+with builtins;
 let
   outputs = builtins.getFlake (toString ./.);
-  system = "x86_64-linux";
-  pkgs = import outputs.inputs.nixpkgs { inherit system; };
-  host-workstation = (pkgs.callPackage ./scripts/force_cached.nix {}) outputs.packages.x86_64-linux.host-workstation;
-  host-hypervisor = (pkgs.callPackage ./scripts/force_cached.nix {}) outputs.packages.x86_64-linux.host-hypervisor;
-in host-workstation // host-hypervisor
+  pkgs = outputs.inputs.nixpkgs;
+
+  isHost = n: pkgs.lib.strings.hasPrefix "host" n;
+  isDerivation = p: isAttrs p && p ? type && p.type == "derivation";
+  isBuildable = p: !(p.meta.broken or false);
+  isCacheable = p: !(p.preferLocalBuild or false);
+  shouldRecurseForDerivations = p:
+    isAttrs p && p.recurseForDerivations or false;
+
+  nameValuePair = n: v: {
+    name = n;
+    value = v;
+  };
+
+  concatMap = builtins.concatMap or (f: xs: concatLists (map f xs));
+
+  flattenPkgs = s:
+    let
+      f = p:
+        if shouldRecurseForDerivations p then
+          flattenPkgs p
+        else if isDerivation p then
+          [ p ]
+        else
+          [ ];
+    in concatMap f (attrValues s);
+
+  outputsOf = p: map (o: p.${o}) p.outputs;
+  hostAttrs = outputs.packages.x86_64-linux;
+
+  hostPkgs = flattenPkgs (listToAttrs (map (n: nameValuePair n hostAttrs.${n})
+    (filter (n: isHost n) (attrNames hostAttrs))));
+in rec {
+  buildPkgs = filter isBuildable hostPkgs;
+  cachePkgs = filter isCacheable buildPkgs;
+
+  buildOutputs = concatMap outputsOf buildPkgs;
+  cacheOutputs = concatMap outputsOf cachePkgs;
+}
