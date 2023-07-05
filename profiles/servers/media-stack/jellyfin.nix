f2b81a1b99f7da7616061a3de307c244a8b2aaea
Dmitriy Kholkin <ataraxiadev@ataraxiadev.com>
2023-07-05 20:44:06 +0300

    change jellyfin container

--- profiles/servers/media-stack/jellyfin.nix
+++ profiles/servers/media-stack/jellyfin.nix
@@ -2,18 +2,26 @@
 let
   backend = config.virtualisation.oci-containers.backend;
   nas-path = "/media/nas/media-stack";
+  renderGid = toString config.users.groups.render.gid;
+  videoGid = toString config.users.groups.video.gid;
+  inputGid = toString config.users.groups.input.gid;
 in {
   virtualisation.oci-containers.containers.jellyfin = {
     autoStart = true;
-    image = "cr.hotio.dev/hotio/jellyfin:release-10.8.9-1";
+    image = "lscr.io/linuxserver/jellyfin:10.8.10";
     environment = {
       PUID = "1000";
       PGID = "100";
       UMASK = "002";
       TZ = "Europe/Moscow";
+      DOCKER_MODS = "linuxserver/mods:jellyfin-opencl-intel";
     };
-    extraOptions = [ "--pod=media-stack" "--device=/dev/dri:/dev/dri" ];
-    # ports = [ "127.0.0.1:8096:8096/tcp" ];
+    extraOptions = [
+      "--pod=media-stack"
+      "--device=/dev/dri/renderD128:/dev/dri/renderD128"
+      "--group-add=${renderGid},${videoGid},${inputGid}"
+      # "--privileged"
+    ];
     volumes = [
       "${nas-path}/configs/jellyfin:/config"
       "${nas-path}/media:/data/media"
