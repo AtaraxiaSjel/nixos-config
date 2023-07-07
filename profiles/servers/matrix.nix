750a106e0b24d7aed8f29cce4b52554326cf6f26
Dmitriy Kholkin <ataraxiadev@ataraxiadev.com>
2023-07-08 02:17:59 +0300

    recreate matrix vm with fedora

--- profiles/servers/matrix.nix
+++ profiles/servers/matrix.nix
@@ -1,15 +1,15 @@
 { config, lib, pkgs, ... }: {
-  virtualisation.libvirt.guests.arch-matrix = {
+  virtualisation.libvirt.guests.fedora-synapse = {
+    autoStart = true;
     user = config.mainuser;
     group = "libvirtd";
-    # I need more ram... temporarily disabled
-    autoStart = true;
+    uefi = true;
     memory = 2 * 1024;
     cpu = {
-      sockets = 1; cores = 1; threads = 1;
+      sockets = 1; cores = 1; threads = 2;
     };
     devices = {
-      disks = [ { diskFile = "/media/nas/libvirt/images/matrix-server.qcow2"; } ];
+      disks = [ { diskFile = "/media/nas/libvirt/images/fedora-synapse.img"; type = "raw"; } ];
       network = {
         macAddress = "00:16:3e:5b:49:bf";
         interfaceType = "bridge";
