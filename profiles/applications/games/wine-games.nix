15dc17f77d874d0f38f13f0303db96e54aa1279d
Dmitriy Kholkin <ataraxiadev@ataraxiadev.com>
2023-11-22 06:09:29 +0300

    remove lutris

--- profiles/applications/games/wine-games.nix
+++ profiles/applications/games/wine-games.nix
@@ -1,12 +1,13 @@
 { pkgs, lib, config, ... }: {
   home-manager.users.${config.mainuser}.home.packages = [
-    pkgs.lutris pkgs.bottles
+    # pkgs.lutris
+    pkgs.bottles
     pkgs.osu-lazer-bin
     pkgs.realrtcw
   ];
   persist.state.homeDirectories = [
-    ".config/lutris"
-    ".local/share/lutris"
+    # ".config/lutris"
+    # ".local/share/lutris"
     ".local/share/bottles"
     ".local/share/osu"
   ];
