b93fbe3a062c2fb2f01028be78e16a0f31d85a2e
Dmitriy Kholkin <ataraxiadev@ataraxiadev.com>
2024-01-21 17:04:01 +0300

    connect to bathist only from local

--- profiles/servers/battery-historian.nix
+++ profiles/servers/battery-historian.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }: {
   virtualisation.oci-containers.containers.battery-historian = {
     autoStart = true;
-    ports = [ "127.0.0.1:9999:9999" ];
+    ports = [ "0.0.0.0:9999:9999" ];
     image = "gcr.io/android-battery-historian/stable:3.0";
   };
 }
