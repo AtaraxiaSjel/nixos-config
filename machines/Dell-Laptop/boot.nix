f7f371f1060aacf4fe27db031bb249398c185a52
Dmitriy Kholkin <ataraxiadev@ataraxiadev.com>
2024-02-06 23:58:35 +0300

    fix zfs on dell-laptop

--- machines/Dell-Laptop/boot.nix
+++ machines/Dell-Laptop/boot.nix
@@ -3,7 +3,7 @@ let
   zfs_arc_max = toString (2 * 1024 * 1024 * 1024);
 in {
   boot = {
-    zfs.enableUnstable = false; # For now, unstable zfs will downgrade kernel
+    zfs.enableUnstable = true;
     kernelPackages = pkgs.linuxPackages_lqx;
 
     initrd = {
