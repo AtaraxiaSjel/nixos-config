ee592bc3b970f0489fb103b51b9660b8a8b6b0c5
Dmitriy Kholkin <ataraxiadev@ataraxiadev.com>
2024-03-04 00:01:05 +0300

    remove proxy for radarr container

--- profiles/servers/media-stack/radarr.nix
+++ profiles/servers/media-stack/radarr.nix
@@ -9,8 +9,6 @@ in {
       PGID = "100";
       UMASK = "002";
       TZ = "Europe/Moscow";
-      HTTP_PROXY = "http://192.168.0.6:8888";
-      HTTPS_PROXY = "http://192.168.0.6:8888";
     };
     extraOptions = [ "--pod=media-stack" ];
     image = "docker.io/linuxserver/radarr:5.2.6";
