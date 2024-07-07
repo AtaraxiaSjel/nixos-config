aa7a75a09a61800d0b2a0bff5d189ad4f8897184
Dmitriy Kholkin <ataraxiadev@ataraxiadev.com>
2024-07-07 17:03:21 +0300

    gaming stuff

--- profiles/applications/games/hoyo.nix
+++ profiles/applications/games/hoyo.nix
@@ -5,6 +5,11 @@
   programs.honkers-railway-launcher.enable = true;
   networking.mihoyo-telemetry.block = lib.mkForce true;
 
+  networking.extraHosts = ''
+    0.0.0.0 globaldp-prod-os01.zenlesszonezero.com
+    0.0.0.0 apm-log-upload.mihoyo.com
+  '';
+
   persist.state.homeDirectories = [
     ".local/share/honkers-railway-launcher"
   ];
--- profiles/applications/games/wine-games.nix
+++ profiles/applications/games/wine-games.nix
@@ -1,10 +1,12 @@
 { pkgs, config, ... }: {
   home-manager.users.${config.mainuser}.home.packages = [
     pkgs.bottles
+    pkgs.heroic
     pkgs.osu-lazer-bin
     pkgs.realrtcw
   ];
   persist.state.homeDirectories = [
+    ".config/heroic"
     ".local/share/bottles"
     ".local/share/osu"
   ];
