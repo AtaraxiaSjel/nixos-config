b11bf4a173190bb8bac0b3fcaf5574dba9f86633
Dmitriy Kholkin <ataraxiadev@ataraxiadev.com>
2024-09-19 23:56:36 +0300

    games stuff

--- profiles/applications/games/wine-games.nix
+++ profiles/applications/games/wine-games.nix
@@ -2,14 +2,19 @@
   home-manager.users.${config.mainuser}.home.packages = [
     pkgs.bottles
     pkgs.heroic
+    pkgs.lutris
     pkgs.osu-lazer-bin
+    pkgs.protonup-qt
     pkgs.realrtcw
     pkgs.umu-launcher
+    pkgs.wine
   ];
   persist.state.homeDirectories = [
     ".config/heroic"
     ".local/share/bottles"
+    ".local/share/lutris"
     ".local/share/osu"
+    ".local/share/reshade"
     ".local/share/umu"
   ];
 }
