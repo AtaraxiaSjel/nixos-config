c60116fcada5f87eb5c181d4cd70193d0e98647b
Dmitriy Kholkin <ataraxiadev@ataraxiadev.com>
2024-10-24 03:57:05 +0300

    stable kernel for ISO

--- machines/Flakes-ISO/default.nix
+++ machines/Flakes-ISO/default.nix
@@ -1,6 +1,6 @@
 { modulesPath, lib, inputs, pkgs, config, ... }: {
   imports = with inputs.self; [
-    "${toString modulesPath}/installer/cd-dvd/installation-cd-graphical-plasma5-new-kernel.nix"
+    "${toString modulesPath}/installer/cd-dvd/installation-cd-graphical-plasma5.nix"
     # "${toString modulesPath}/installer/cd-dvd/installation-cd-base.nix"
   ];
 
