fbb7636bff5cc91b82c67ef6f2ce1b8dce0b5a7e
Dmitriy Kholkin <ataraxiadev@ataraxiadev.com>
2025-02-12 22:04:32 +0300

    use nixpkgs aavmf

--- profiles/virtualisation.nix
+++ profiles/virtualisation.nix
@@ -77,16 +77,5 @@ with config.deviceSpecific; {
       "/var/lib/libvirt"
       "/var/lib/containers"
     ];
-
-    # cross compilation of aarch64 uefi currently broken
-    # link existing extracted from fedora package
-    system.activationScripts.aarch64-ovmf = lib.mkIf (!isServer) {
-      text = ''
-        rm -f /run/libvirt/nix-ovmf/AAVMF_*
-        mkdir -p /run/libvirt/nix-ovmf || true
-        ${pkgs.zstd}/bin/zstd -d ${../misc/AAVMF_CODE.fd.zst} -o /run/libvirt/nix-ovmf/AAVMF_CODE.fd
-        ${pkgs.zstd}/bin/zstd -d ${../misc/AAVMF_VARS.fd.zst} -o /run/libvirt/nix-ovmf/AAVMF_VARS.fd
-      '';
-    };
   };
 }
