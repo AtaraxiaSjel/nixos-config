c60d1f1ea888be4e789f7d2a5b8f1ff4212f3979
Dmitriy Kholkin <ataraxiadev@ataraxiadev.com>
2025-02-16 22:21:41 +0300

    use linuxserver kavita container

--- profiles/servers/media-stack/kavita.nix
+++ profiles/servers/media-stack/kavita.nix
@@ -4,18 +4,22 @@ let
 in {
   virtualisation.oci-containers.containers.kavita = {
     autoStart = true;
-    image = "docker.io/jvmilazz0/kavita:0.8.3";
+    # Tags: 0.8.4, version-v0.8.4.2, v0.8.4.2-ls63
+    image = "docker.io/linuxserver/kavita@sha256:03b68c3137f986dc8a9b126c9e0fd7f356e0e9c9e83ffa8fa6356cd028288c8a";
     environment = {
       PUID = "1000";
       PGID = "100";
+      TZ = "Europe/Moscow";
+      DOTNET_SYSTEM_GLOBALIZATION_INVARIANT = "true";
     };
     extraOptions = [ "--pod=media-stack" ];
     volumes = [
-      "/etc/localtime:/etc/localtime:ro"
-      "${nas-path}/configs/kavita:/kavita/config"
-      "${nas-path}/media/manga:/manga/manga"
-      "${nas-path}/media/books:/manga/books"
-      "${nas-path}/media/comics:/manga/comics"
+      "${nas-path}/configs/kavita:/config"
+      "${nas-path}/media/books:/data/books"
+      "${nas-path}/media/comics:/data/comics"
+      "${nas-path}/media/fanfics:/data/fanfics"
+      "${nas-path}/media/manga:/data/manga"
+      "${nas-path}/media/novels:/data/novels"
     ];
   };
 }
