05db81007932fd21eff57fad0c159b3f2eafa379
Dmitriy Kholkin <ataraxiadev@ataraxiadev.com>
2025-03-10 18:29:45 +0300

    feat: module for role system

--- modules/nixos/roles/default.nix
+++ modules/nixos/roles/default.nix
@@ -5,11 +5,14 @@
 }:
 let
   inherit (lib)
+    mkDefault
     mkIf
     mkMerge
     mkOption
+    recursiveUpdate
     types
     ;
+
   role = config.ataraxia.defaults.role;
 in
 {
@@ -25,9 +28,41 @@ in
     };
   };
 
-  config = mkMerge [
-    (mkIf (role == "base") {
-      ataraxia.defaults.nix.enable = true;
-    })
-  ];
+  config =
+    let
+      baseRole = {
+        ataraxia.defaults.lix.enable = mkDefault true;
+        ataraxia.defaults.nix.enable = mkDefault true;
+
+        persist.enable = mkDefault true;
+
+        boot.initrd.systemd.enable = mkDefault true;
+        services.userborn.enable = mkDefault true;
+        system.rebuild.enableNg = mkDefault true;
+        system.switch.enableNg = mkDefault true;
+        system.etc.overlay.enable = mkDefault true;
+        system.etc.overlay.mutable = mkDefault true;
+
+        zramSwap = {
+          enable = true;
+          algorithm = "zstd";
+          memoryPercent = 100;
+        };
+      };
+      serverRole = recursiveUpdate baseRole {
+        time.timeZone = "Etc/UTC";
+      };
+      desktopRole = recursiveUpdate baseRole {
+        location = {
+          provider = "manual";
+          latitude = 48;
+          longitude = 44;
+        };
+      };
+    in
+    mkMerge [
+      (mkIf (role == "base") baseRole)
+      (mkIf (role == "server") serverRole)
+      (mkIf (role == "desktop") desktopRole)
+    ];
 }
