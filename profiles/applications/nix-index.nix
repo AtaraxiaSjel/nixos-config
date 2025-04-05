d6c43647c68604dff90c76e3dbf0b7630302d0cf
Dmitriy Kholkin <ataraxiadev@ataraxiadev.com>
2025-04-05 20:31:55 +0300

    temporarily disable nix-index-update service

--- profiles/applications/nix-index.nix
+++ profiles/applications/nix-index.nix
@@ -1,21 +1,21 @@
 { config, lib, pkgs, ... }: {
-  systemd.services = let
-    service = user: group: {
-      wantedBy = [ "multi-user.target" ];
-      wants = [ "network-online.target" ];
-      after = [ "network-online.target" ];
-      path = [ pkgs.nix-index-update ];
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = lib.getExe pkgs.nix-index-update;
-        User = user;
-        Group = group;
-      };
-    };
-  in {
-    "nix-index-update-root" = service "root" "root";
-    "nix-index-update-${config.mainuser}" = service config.mainuser "users";
-  };
+  # systemd.services = let
+  #   service = user: group: {
+  #     wantedBy = [ "multi-user.target" ];
+  #     wants = [ "network-online.target" ];
+  #     after = [ "network-online.target" ];
+  #     path = [ pkgs.nix-index-update ];
+  #     serviceConfig = {
+  #       Type = "oneshot";
+  #       ExecStart = lib.getExe pkgs.nix-index-update;
+  #       User = user;
+  #       Group = group;
+  #     };
+  #   };
+  # in {
+  #   "nix-index-update-root" = service "root" "root";
+  #   "nix-index-update-${config.mainuser}" = service config.mainuser "users";
+  # };
   home-manager.users.${config.mainuser} = {
     programs.nix-index = {
       enable = true;
