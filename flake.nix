10036817cca475ef7cae998bd39ff113b01de9df
Dmitriy Kholkin <ataraxiadev@ataraxiadev.com>
2025-07-08 20:03:45 +0300

    fix: add srvos input

--- flake.nix
+++ flake.nix
@@ -64,6 +64,10 @@
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    srvos = {
+      url = "github:nix-community/srvos";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     walker = {
       url = "github:abenz1267/walker";
       inputs.nixpkgs.follows = "nixpkgs";
