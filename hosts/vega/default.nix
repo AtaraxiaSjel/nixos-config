bb5dbdaff88380ced9d9674575214a26b4b2555d
Dmitriy Kholkin <ataraxiadev@ataraxiadev.com>
2026-02-10 03:51:52 +0300

    feat: enable brave and nushell for vega

--- hosts/vega/default.nix
+++ hosts/vega/default.nix
@@ -49,7 +49,9 @@ in
   # Home-manager
   home-manager.users.${defaultUser} = {
     ataraxia.defaults.role = "laptop";
+    ataraxia.programs.brave.enable = true;
     ataraxia.programs.mangohud.enable = true;
+    ataraxia.programs.nushell.enable = true;
     ataraxia.services.modprobed-db.enable = true;
     ataraxia.theme.catppuccin.enable = true;
 
