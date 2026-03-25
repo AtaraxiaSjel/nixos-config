2cd96958dff1a76cfb7aca83c5a06e51ceab1576
Dmitriy Kholkin <ataraxiadev@ataraxiadev.com>
2026-03-26 00:58:35 +0300

    feat: add warp to cloverleaf and redshift hosts

--- hosts/cloverleaf/proxy.nix
+++ hosts/cloverleaf/proxy.nix
@@ -1,4 +1,12 @@
-{ pkgs, ... }:
+{
+  config,
+  pkgs,
+  secretsDir,
+  ...
+}:
+let
+  hostname = config.networking.hostName;
+in
 {
   networking.firewall.allowedTCPPorts = [
     80
@@ -42,6 +50,7 @@
         tcp-request inspect-delay 5s
         tcp-request content accept if { req_ssl_hello_type 1 }
         use_backend backend_vless if { req_ssl_sni -i panel.ataraxiadev.com }
+        use_backend backend_vless_bridge if { req_ssl_sni -i cloud-02.ataraxiadev.com }
         use_backend backend_telemt if { req_ssl_sni -i panel-tg.ataraxiadev.com }
         default_backend backend_caddy_https
 
@@ -55,11 +64,15 @@
 
       backend backend_vless
         mode tcp
-        server vless 127.0.0.1:10443 send-proxy-v2 check
+        server vless 127.0.0.1:10443 send-proxy-v2
+
+      backend backend_vless_bridge
+        mode tcp
+        server vless_bridge 127.0.0.1:10444 send-proxy-v2
 
       backend backend_telemt
         mode tcp
-        server telemt 127.0.0.1:20443 send-proxy-v2 check
+        server telemt 127.0.0.1:20443 send-proxy-v2
     '';
   };
   services.caddy = {
@@ -90,6 +103,10 @@
         reverse_proxy 127.0.0.1:8081
         header -Server
       }
+      https://cloud-02.ataraxiadev.com {
+        reverse_proxy 127.0.0.1:8081
+        header -Server
+      }
       https://panel-tg.ataraxiadev.com {
         reverse_proxy 127.0.0.1:8081
         header -Server
@@ -102,5 +119,51 @@
         abort
       }
     '';
+  };
+
+  networking.firewall.checkReversePath = "loose";
+  sops.secrets."warp-${hostname}" = {
+    sopsFile = secretsDir + /${hostname}/warp.yaml;
+    mode = "640";
+    owner = "systemd-network";
+    group = "systemd-network";
+  };
+  systemd.network = {
+    networks."50-warp" = {
+      matchConfig.Name = "warp";
+      address = [
+        "172.16.0.2/32"
+        "2606:4700:110:8bbf:1df3:7f98:71ec:6c2d/128"
+      ];
+      routingPolicyRules = [
+        {
+          FirewallMark = 51;
+          Table = 51;
+        }
+      ];
+      linkConfig.ActivationPolicy = "up";
+    };
+    netdevs."50-warp" = {
+      netdevConfig = {
+        Kind = "wireguard";
+        Name = "warp";
+        MTUBytes = "1280";
+      };
+      wireguardConfig = {
+        PrivateKeyFile = config.sops.secrets."warp-${hostname}".path;
+      };
+      wireguardPeers = [
+        {
+          PublicKey = "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=";
+          AllowedIPs = [
+            "0.0.0.0/0"
+            "::/0"
+          ];
+          Endpoint = "162.159.192.1:2408";
+          PersistentKeepalive = 25;
+          RouteTable = 51;
+        }
+      ];
+    };
   };
 }
