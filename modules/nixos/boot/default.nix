a320fc4697c79f1ee2f4b9afce855e9127cf4437
Dmitriy Kholkin <ataraxiadev@ataraxiadev.com>
2026-09-17 22:32:52 +0300

    refactor: boot module

--- modules/nixos/boot/default.nix
+++ modules/nixos/boot/default.nix
@@ -9,6 +9,7 @@ let
     mkDefault
     mkEnableOption
     mkIf
+    mkMerge
     mkOption
     mkOverride
     types
@@ -17,14 +18,16 @@ let
   cfg = config.ataraxia.defaults.boot;
 
   cachyosKernel =
-    if cfg.kernelLevel == "v3" then
+    if cfg.kernelLevel == "v2" then
+      pkgs.cachyosKernels.linuxPackages-cachyos-latest-lto-x86_64-v2
+    else if cfg.kernelLevel == "v3" then
       pkgs.cachyosKernels.linuxPackages-cachyos-latest-lto-x86_64-v3
     else if cfg.kernelLevel == "v4" then
       pkgs.cachyosKernels.linuxPackages-cachyos-latest-lto-x86_64-v4
     else if cfg.kernelLevel == "zen4" then
       pkgs.cachyosKernels.linuxPackages-cachyos-latest-lto-zen4
     else
-      pkgs.cachyosKernels.linuxPackages-cachyos-latest-lto-x86_64-v2;
+      pkgs.cachyosKernels.linuxPackages-cachyos-latest-lto;
   # cachyosPatched = cachyosKernel;
 
   # kernel = pkgs.cachyosKernels.linux-cachyos-latest-lto-zen4.override {
@@ -91,53 +94,55 @@ in
     patchGdtLimit = mkEnableOption "Patch GDT Limit to 0x7F";
   };
 
-  config = mkIf cfg.enable {
-    boot = {
-      loader = {
-        efi.efiSysMountPoint = "/efi";
-        efi.canTouchEfiVariables = mkDefault true;
-        limine = {
-          enable = mkDefault true;
-          enableEditor = mkDefault false;
-          maxGenerations = mkDefault 10;
-          validateChecksums = mkDefault true;
-          panicOnChecksumMismatch = mkDefault true;
-          efiSupport = mkDefault true;
-          efiInstallAsRemovable = mkDefault false;
-          biosSupport = mkDefault false;
+  config = mkMerge [
+    (mkIf cfg.enable {
+      boot = {
+        loader = {
+          efi.efiSysMountPoint = "/efi";
+          efi.canTouchEfiVariables = mkDefault true;
+          limine = {
+            enable = mkDefault true;
+            enableEditor = mkDefault false;
+            maxGenerations = mkDefault 10;
+            validateChecksums = mkDefault true;
+            panicOnChecksumMismatch = mkDefault true;
+            efiSupport = mkDefault true;
+            efiInstallAsRemovable = mkDefault false;
+            biosSupport = mkDefault false;
+          };
+          grub.enable = mkDefault false;
+          systemd-boot.enable = mkDefault false;
+          timeout = mkDefault 4;
         };
-        grub.enable = mkDefault false;
-        systemd-boot.enable = mkDefault false;
-        timeout = mkDefault 4;
+
+        kernelParams = [
+          "kvm.ignore_msrs=1"
+          "kvm.report_ignored_msrs=0"
+          "nofb"
+          "rd.systemd.show_status=auto"
+          "rd.udev.log_priority=3"
+          "zswap.enabled=0"
+        ];
+
+        kernelPackages = mkOverride 910 pkgs.linuxPackages_xanmod_latest;
+        zfs.package = mkOverride 910 pkgs.zfs_unstable;
+
+        consoleLogLevel = 3;
+
+        kernel.sysctl = mkIf config.zramSwap.enable {
+          "vm.swappiness" = 100;
+          "vm.vfs_cache_pressure" = 200;
+          "vm.dirty_background_ratio" = 1;
+          "vm.dirty_ratio" = 40;
+          "vm.page-cluster" = 0;
+        };
+
+        tmp.cleanOnBoot = !config.boot.tmp.useTmpfs;
       };
-
-      kernelParams = [
-        "kvm.ignore_msrs=1"
-        "kvm.report_ignored_msrs=0"
-        "nofb"
-        "rd.systemd.show_status=auto"
-        "rd.udev.log_priority=3"
-        "zswap.enabled=0"
-      ];
-
-      kernelPackages = mkOverride 900 (
-        if cfg.cachyosKernel then cachyosFinal else pkgs.linuxPackages_xanmod_latest
-      );
-      zfs.package = mkOverride 900 (
-        if cfg.cachyosKernel then config.boot.kernelPackages.zfs_cachyos else pkgs.zfs_unstable
-      );
-
-      consoleLogLevel = 3;
-
-      kernel.sysctl = mkIf config.zramSwap.enable {
-        "vm.swappiness" = 100;
-        "vm.vfs_cache_pressure" = 200;
-        "vm.dirty_background_ratio" = 1;
-        "vm.dirty_ratio" = 40;
-        "vm.page-cluster" = 0;
-      };
-
-      tmp.cleanOnBoot = !config.boot.tmp.useTmpfs;
-    };
-  };
+    })
+    (mkIf cfg.cachyosKernel {
+      boot.kernelPackages = mkOverride 900 cachyosFinal;
+      boot.zfs.package = mkOverride 900 config.boot.kernelPackages.zfs_cachyos;
+    })
+  ];
 }
